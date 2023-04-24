f76f1f9e2e834853f55da6dab64759fdb0079b2e
Sanaesb <sbaitisanaa99@gmail.com>
2023-04-24 11:46:53 -0700

    Commito

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - entry point
  *
- * Description: print the alue of n status:
+ * Description: print the value of n status:
  *              greater than, is zero and is not less than 6.
  *
  * Return:0 (success)
@@ -21,6 +21,6 @@ int main(void)
 	else if (digit == 0)
 	printf("Last digit of %i is %i and is 0\n", n, digit);
 	else if (digit < 6 && digit !=0)
-		printf("Last digit of %i is %i and is less than 6\n", n, digit);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
